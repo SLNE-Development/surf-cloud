23f485cff59c3b417b417d2c21e8d16faa5485e8
twisti <denizgebauer@web.de>
2024-11-23 16:59:22 +0100

    docs: enhance CloudPlayerManager.kt javadocs

--- surf-cloud-api/surf-cloud-api-common/src/main/kotlin/dev/slne/surf/cloud/api/common/player/CloudPlayerManager.kt
+++ surf-cloud-api/surf-cloud-api-common/src/main/kotlin/dev/slne/surf/cloud/api/common/player/CloudPlayerManager.kt
@@ -10,22 +10,31 @@ import java.util.*
 interface CloudPlayerManager {
 
     /**
-     * Get a player by their UUID. This will return null if the player is not online.
-     * @param uuid The UUID of the player to get.
-     * @return The player, or null if they are not online or the provided UUID is null.
+     * Retrieves a player by their UUID. Returns `null` if the player is not online or if the provided UUID is `null`.
+     *
+     * @param uuid The UUID of the player to retrieve.
+     * @return The [CloudPlayer] instance if the player is online, or `null` otherwise.
      */
     fun getPlayer(uuid: UUID?): CloudPlayer?
 
     companion object {
+        /**
+         * The singleton instance of the [CloudPlayerManager].
+         */
         val instance = requiredService<CloudPlayerManager>()
     }
 }
 
+/**
+ * A global reference to the singleton [CloudPlayerManager] instance.
+ */
 val playerManager get() = CloudPlayerManager.instance
 
 /**
- * Get the CloudPlayer for this Audience.
- * @return The CloudPlayer, or null if the Audience is not a player.
+ * Converts an [Audience] to its corresponding [CloudPlayer].
+ *
+ * @return The [CloudPlayer] if the [Audience] is a player,
+ * or `null` if the [Audience] is not a player or cannot be resolved.
  */
 fun Audience?.toCloudPlayer(): CloudPlayer? {
     return playerManager.getPlayer(this?.pointers()?.get(Identity.UUID)?.orElse(null))

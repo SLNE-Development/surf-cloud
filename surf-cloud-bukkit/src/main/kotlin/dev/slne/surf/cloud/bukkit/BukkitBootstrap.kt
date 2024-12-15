018832c72c53895ddce034cf6f4adf33fbaeee41
twisti <denizgebauer@web.de>
2024-12-15 11:50:12 +0100

    Refactor bootstrap call to use bukkitCloudInstance.
    Replaces BukkitNettyManager with bukkitCloudInstance in the bootstrap process to align with updated design. Also adds the "unused" suppression annotation to the class declaration.

--- surf-cloud-bukkit/src/main/kotlin/dev/slne/surf/cloud/bukkit/BukkitBootstrap.kt
+++ surf-cloud-bukkit/src/main/kotlin/dev/slne/surf/cloud/bukkit/BukkitBootstrap.kt
@@ -1,14 +1,13 @@
 package dev.slne.surf.cloud.bukkit
 
-import dev.slne.surf.cloud.bukkit.netty.BukkitNettyManager
 import io.papermc.paper.plugin.bootstrap.BootstrapContext
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap
 import kotlinx.coroutines.runBlocking
 
-@Suppress("UnstableApiUsage")
-class BukkitBootstrap: PluginBootstrap {
+@Suppress("UnstableApiUsage", "unused")
+class BukkitBootstrap : PluginBootstrap {
 
     override fun bootstrap(context: BootstrapContext) = runBlocking {
-        BukkitNettyManager.bootstrap()
+        bukkitCloudInstance.bootstrap()
     }
 }

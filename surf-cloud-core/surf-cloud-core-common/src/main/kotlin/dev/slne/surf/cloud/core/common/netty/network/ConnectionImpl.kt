8e218fd38e9507fbcc8c9166d6fc7e96ad9b16e3
Ammo <ammo@slne.dev>
2024-12-28 22:57:20 +0100

    add ReconnectOnCloseListener.kt which still has to be implemented into the pipeline somwhere and also needs its globalscope fixed?

--- surf-cloud-core/surf-cloud-core-common/src/main/kotlin/dev/slne/surf/cloud/core/common/netty/network/ConnectionImpl.kt
+++ surf-cloud-core/surf-cloud-core-common/src/main/kotlin/dev/slne/surf/cloud/core/common/netty/network/ConnectionImpl.kt
@@ -255,6 +255,7 @@ class ConnectionImpl(
                         is ServerboundPreRunningAcknowledgedPacket -> listener.handlePreRunningAcknowledged(
                             msg
                         )
+
                         is ServerboundRequestContinuation -> listener.handleRequestContinuation(msg)
 
                         else -> error("Unexpected packet $msg")
@@ -345,6 +346,7 @@ class ConnectionImpl(
                         is ClientboundPreRunningFinishedPacket -> listener.handlePreRunningFinished(
                             msg
                         )
+
                         is ClientboundReadyToRunPacket -> listener.handleReadyToRun(msg)
 
                         else -> error("Unexpected packet $msg")
@@ -787,6 +789,17 @@ class ConnectionImpl(
             channel.close()
             this.disconnectionDetails = reason
         }
+    }
+
+    suspend fun reconnect(reason: DisconnectionDetails) {
+        val host = cloudConfig.connectionConfig.nettyConfig.host
+        val port = cloudConfig.connectionConfig.nettyConfig.port
+        val epoll = Epoll.isAvailable()
+
+        val address = InetSocketAddress(host, port)
+
+        disconnect(reason)
+        connect(address, epoll, this)
     }
 
     fun clearPacketQueue() {

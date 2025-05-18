2465388c505d5de7308b199c9aa071e64eef4afc
twisti <denizgebauer@web.de>
2025-05-18 14:57:58 +0200

    feat(queue): optimize expired UUID collection with read-write lock separation

--- surf-cloud-standalone/src/main/kotlin/dev/slne/surf/cloud/standalone/server/queue/timeout/ReentrantRedWriteLockQueueTimeoutRegistry.kt
+++ surf-cloud-standalone/src/main/kotlin/dev/slne/surf/cloud/standalone/server/queue/timeout/ReentrantRedWriteLockQueueTimeoutRegistry.kt
@@ -2,9 +2,11 @@ package dev.slne.surf.cloud.standalone.server.queue.timeout
 
 import dev.slne.surf.surfapi.core.api.util.mutableObject2LongMapOf
 import dev.slne.surf.surfapi.core.api.util.toObjectSet
+import it.unimi.dsi.fastutil.objects.ObjectSet
 import org.springframework.stereotype.Component
 import java.util.*
 import java.util.concurrent.locks.ReentrantReadWriteLock
+import kotlin.concurrent.read
 import kotlin.concurrent.write
 
 
@@ -15,13 +17,15 @@ class ReentrantRedWriteLockQueueTimeoutRegistry : QueueTimeoutRegistry {
 
     override fun put(uuid: UUID, untilMillis: Long) = lock.write { map[uuid] = untilMillis }
     override fun remove(uuid: UUID): Unit = lock.write { map.removeLong(uuid) }
-    override fun collectExpired(now: Long) = lock.write {
-        map.object2LongEntrySet().fastIterator().asSequence()
-            .filter { it.longValue <= now }
-            .map { it.key }
-            .toObjectSet()
-            .also {
-                it.forEach { uuid -> map.removeLong(uuid) }
-            }
+    override fun collectExpired(now: Long): ObjectSet<UUID> {
+        val expired = lock.read {
+            map.object2LongEntrySet().fastIterator().asSequence()
+                .filter { it.longValue <= now }
+                .map { it.key }
+                .toObjectSet()
+        }
+
+        lock.write { expired.forEach { map.removeLong(it) } }
+        return expired
     }
 }

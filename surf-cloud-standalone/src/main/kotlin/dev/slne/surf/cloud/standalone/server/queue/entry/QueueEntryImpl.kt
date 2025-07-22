f6d056e92f0a1987270bdb9d3eb197cd82665cd6
twisti <denizgebauer@web.de>
2025-07-22 22:52:31 +0200

    refactor(QueueEntryImpl): replace volatile awaitingConnection variable with AtomicBoolean for thread safety

--- surf-cloud-standalone/src/main/kotlin/dev/slne/surf/cloud/standalone/server/queue/entry/QueueEntryImpl.kt
+++ surf-cloud-standalone/src/main/kotlin/dev/slne/surf/cloud/standalone/server/queue/entry/QueueEntryImpl.kt
@@ -1,8 +1,11 @@
 package dev.slne.surf.cloud.standalone.server.queue.entry
 
+import dev.slne.surf.cloud.api.common.util.getValue
+import dev.slne.surf.cloud.api.common.util.setValue
 import dev.slne.surf.cloud.api.server.queue.QueueEntry
 import dev.slne.surf.cloud.standalone.config.standaloneConfig
 import java.util.*
+import java.util.concurrent.atomic.AtomicBoolean
 
 data class QueueEntryImpl(
     val handle: PlayerQueueHandle,
@@ -11,8 +14,7 @@ data class QueueEntryImpl(
     override val bypassQueue: Boolean,
     override val preferredServerUid: Long? = null,
 ) : QueueEntry {
-    @Volatile
-    override var awaitingConnection = false
+    override var awaitingConnection by AtomicBoolean()
     override var connectionAttempts = 0
 
     override fun hasConnectionAttemptsLeft() =

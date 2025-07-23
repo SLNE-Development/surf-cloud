d85f96542f2618452064edae67c485d650e5cd88
twisti <denizgebauer@web.de>
2025-07-23 22:32:23 +0200

    feat: add ComponentColumnType for handling Component serialization in database

---
+++ surf-cloud-api/surf-cloud-api-server/src/main/kotlin/dev/slne/surf/cloud/api/server/exposed/columns/ComponentColumnType.kt
@@ -0,0 +1,26 @@
+package dev.slne.surf.cloud.api.server.exposed.columns
+
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.ColumnType
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.vendors.currentDialect
+import java.nio.ByteBuffer
+
+class ComponentColumnType : ColumnType<Component>() {
+    override fun sqlType() = currentDialect.dataTypeProvider.largeTextType()
+
+    override fun valueFromDB(value: Any): Component = when (value) {
+        is Component -> value
+        is String -> GsonComponentSerializer.gson().deserialize(value)
+        is ByteArray -> GsonComponentSerializer.gson().deserialize(value.decodeToString())
+        is ByteBuffer -> GsonComponentSerializer.gson().deserialize(value.array().decodeToString())
+        else -> error("Unexpected value of type Component: $value of ${value::class.qualifiedName}")
+    }
+
+    override fun nonNullValueToString(value: Component): String =
+        GsonComponentSerializer.gson().serialize(value)
+}
+
+fun Table.component(name: String): Column<Component> = registerColumn(name, ComponentColumnType())

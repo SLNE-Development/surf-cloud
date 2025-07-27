58c9f7b21c9589534ba8ece50c28e4f489017a3b
twisti <denizgebauer@web.de>
2025-07-27 17:04:08 +0200

    feat: exclude kotlinx-serialization-core from runtimeClasspath

--- buildSrc/src/main/kotlin/exclude-kotlin.gradle.kts
+++ buildSrc/src/main/kotlin/exclude-kotlin.gradle.kts
@@ -2,5 +2,6 @@ configurations {
     "runtimeClasspath" {
         exclude("org.jetbrains.kotlin", "kotlin-stdlib")
         exclude("org.jetbrains.kotlin", "kotlin-reflect")
+        exclude("org.jetbrains.kotlinx", "kotlinx-serialization-core")
     }
 }

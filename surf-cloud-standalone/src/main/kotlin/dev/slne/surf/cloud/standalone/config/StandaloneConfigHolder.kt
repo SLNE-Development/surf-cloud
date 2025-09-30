6cf981a2180ceca2f9be9f8fb6c6dbbe4aa319a9
twisti <denizgebauer@web.de>
2025-09-30 20:13:00 +0200

    feat: add support for Flyway configuration overrides and standalone application customization
    - Introduced `FlywayDisabler` to handle dynamic Flyway configuration based on `StandaloneConfigHolder`.
    - Enhanced `StandaloneConfigHolder` to include Flyway-specific settings and a global configuration accessor.
    - Updated `PluginFlywayConfigurationCustomizer` with Flyway-related application customization.
    - Added `CloudSpringApplicationConfiguration` interface for modular Spring application configuration.
    - Centralized Flyway configuration logic with caching mechanisms for improved performance and reuse.
    - Refactored database configuration to include additional Flyway options.

--- surf-cloud-standalone/src/main/kotlin/dev/slne/surf/cloud/standalone/config/StandaloneConfigHolder.kt
+++ surf-cloud-standalone/src/main/kotlin/dev/slne/surf/cloud/standalone/config/StandaloneConfigHolder.kt
@@ -1,10 +1,24 @@
 package dev.slne.surf.cloud.standalone.config
 
+import dev.slne.surf.cloud.core.common.CloudCoreInstance
 import dev.slne.surf.cloud.core.common.config.AbstractSurfCloudConfigHolder
 import dev.slne.surf.cloud.core.common.config.ConfigReloadAware
 import org.springframework.beans.factory.ObjectProvider
+import org.springframework.beans.factory.getBean
 import org.springframework.stereotype.Component
 
 @Component
 class StandaloneConfigHolder(reloadAware: ObjectProvider<ConfigReloadAware>) :
-    AbstractSurfCloudConfigHolder<StandaloneConfig>(reloadAware, StandaloneConfig::class.java)
+    AbstractSurfCloudConfigHolder<StandaloneConfig>(reloadAware, StandaloneConfig::class.java) {
+
+    companion object {
+        val holder
+            get() = CloudCoreInstance.internalContext?.getBean<StandaloneConfigHolder>()
+                ?: error("StandaloneConfigHolder is not initialized yet")
+
+        fun isFlywayEnabled(): Boolean = createConfig(StandaloneConfig::class.java)
+            .connectionConfig
+            .databaseConfig
+            .flyway.enabled
+    }
+}
--- surf-cloud-standalone/src/main/kotlin/dev/slne/surf/cloud/standalone/plugin/spring/config/PluginFlywayConfigurationCustomizer.kt
+++ surf-cloud-standalone/src/main/kotlin/dev/slne/surf/cloud/standalone/plugin/spring/config/PluginFlywayConfigurationCustomizer.kt
@@ -1,6 +1,8 @@
 package dev.slne.surf.cloud.standalone.plugin.spring.config
 
 import dev.slne.surf.cloud.api.server.plugin.provider.classloader.SpringPluginClassloader
+import dev.slne.surf.cloud.core.common.spring.CloudChildSpringApplicationConfiguration
+import dev.slne.surf.cloud.standalone.config.StandaloneConfigHolder
 import dev.slne.surf.surfapi.core.api.reflection.Field
 import dev.slne.surf.surfapi.core.api.reflection.SurfProxy
 import dev.slne.surf.surfapi.core.api.reflection.createProxy
@@ -8,6 +10,7 @@ import dev.slne.surf.surfapi.core.api.reflection.surfReflection
 import org.flywaydb.core.api.configuration.ClassicConfiguration
 import org.flywaydb.core.api.configuration.FluentConfiguration
 import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import java.net.URLClassLoader
@@ -17,8 +20,8 @@ import java.net.URLClassLoader
 @Suppress("SpringJavaInjectionPointsAutowiringInspection")
 class PluginFlywayConfigurationCustomizer(
     private val pluginClassloader: SpringPluginClassloader,
-    private val databaseConfig: DatabaseConfig
-) : FlywayConfigurationCustomizer {
+    private val databaseConfig: DatabaseConfig,
+) : FlywayConfigurationCustomizer, CloudChildSpringApplicationConfiguration {
 
     override fun customize(configuration: FluentConfiguration) {
         configuration.table(pluginClassloader.meta.flywayTableName)
@@ -28,6 +31,20 @@ class PluginFlywayConfigurationCustomizer(
         val source = pluginClassloader.source
         config.classLoader =
             URLClassLoader("${source.fileName}-flyway", arrayOf(source.toUri().toURL()), null)
+    }
+
+    override fun configureChildApplication(
+        builder: SpringApplicationBuilder,
+        classLoader: ClassLoader,
+        vararg parentClassLoader: ClassLoader
+    ) {
+        require(classLoader is SpringPluginClassloader)
+        val flywayEnabled = DatabaseConfigConfiguration.getOrCreate(classLoader.meta).flyway.enabled
+            .toBooleanOrElseGet { StandaloneConfigHolder.holder.config.connectionConfig.databaseConfig.flyway.enabled }
+
+        if (!flywayEnabled) {
+            builder.properties("spring.flyway.enabled=false")
+        }
     }
 
     companion object {
